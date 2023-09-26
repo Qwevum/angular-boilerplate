1830ada4aad5c45fd98a4c244845520d398e1777
Qwevum <103947732+Qwevum@users.noreply.github.com>
2023-09-26 18:54:55 -0400

    Update card.js

--- src/card.js
+++ src/card.js
@@ -1,1 +1,13 @@
+const cloneButton = document.getElementById('btn');
+const deleteButton = document.getElementById('delete');
+const titleButton = document.getElementById('title');
 
+
+cloneButton.addEventListener('click', function(){
+  const clonedCard = document.querySelector('.card').cloneNode(true);
+  document.body.appendChild(clonedCard);
+});
+
+deleteButton.addEventListener('click', function(){
+
+})
